a57771284ba452fd66edf0daf886f5bfd3458df9
AnnLilekova <for_berbs@mail.ru>
2021-01-17 15:11:49 +0500

    добавила 1 часть анимации.

---
+++ script.js
@@ -0,0 +1,14 @@
+(function init(){
+    const cards = Array.from(document.querySelectorAll('.card'))
+    cards.forEach((card) => { card.addEventListener('click', clickEvent) })
+}())
+function clickEvent() {
+    constopened = this.classList.contains('open')
+    if (!opened) {
+        this.classList.remove('close')
+        this.classList.add('open')
+    } else{
+        this.classList.remove('open')
+        this.classList.add('close')
+    }
+}
